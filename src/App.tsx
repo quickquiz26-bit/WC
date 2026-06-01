7fcaf4d7e6a25efed0a9e919b897ae8142dd9fc8
quickquiz26-bit <quickquiz26@gmail.com>
2026-06-01 16:07:36 +0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -454,7 +454,7 @@ export default function App() {
                       style={{
                         position: 'absolute',
                         top: '-35%',
-                        left: '-501px',
+                        left: '-481px',
                         right: '1%',
                         bottom: '1px',
                         width: '200%',
